e75f9b456f36799016ed19e71ae67e4879a3ceec
Rodion Martynov <mar.rodion@gmail.com>
2018-08-17 18:15:58 +0300

    Updated preprocessing to include BIO-tags

--- preprocessing.py
+++ preprocessing.py
@@ -1,29 +1,59 @@
-import pandas as pd
+from io import StringIO
 from pathlib import Path
 
-from pandas.errors import MergeError
+import pandas as pd
 
-DATA_PATH = Path('./data')
 
-# result = pd.DataFrame(columns=['token', 'position', 'pos', 'ner', 'part', 'document'])
-res = []
-for part in DATA_PATH.iterdir():
-    for doc in part.iterdir():
-        print(doc)
-        tags = pd.read_csv(doc / 'en.tags', sep='\t', header=None, quoting=3,
-                           names=['token', 'pos', 'lemma', 'ner', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6'])
-        off = pd.read_csv(doc / 'en.tok.off', sep='\s+', header=None, quoting=3,
-                          names=['start_character_offset', 'end_character_offset', 'position', 'token'])
-        try:
-            merged = pd.merge(off, tags, left_index=True, right_index=True, validate='one_to_one').loc[:,
-                     ['token_x', 'position', 'pos', 'ner']]
-        except MergeError:
-            print('merge')
-        merged['part'] = part.parts[-1]
-        merged['document'] = doc.parts[-1]
-        merged.rename(columns={'token_x': 'token'})
-        res.append(merged)
+def load_data(path):
+    res = []
+    for part in path.iterdir():
+        for doc in part.iterdir():
+            print(doc)
+            with (doc / 'en.tags').open('r', encoding='utf-8') as fh:
+                content = fh.read().strip()
+                sentences = content.split('\n\n')
+                for ind, sent in enumerate(sentences):
+                    tokens = pd.read_csv(StringIO(sent), sep='\t', header=None, quoting=3,
+                                         names=['token', 'pos', 'lemma', 'ner', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6'])
+                    # parse only first part of NER tag
+                    tokens['ner'] = tokens.ner.str.split('-').str[0]
+                    tokens = to_conll_iob(tokens)
+                    tokens['part'] = part.parts[-1]
+                    tokens['document'] = doc.parts[-1]
+                    tokens['sentence'] = ind
+                    res.append(tokens)
+                    del tokens
+    df = pd.concat(res)
+    return df
 
-df = pd.concat(res)
-df.columns = ['token', 'position', 'pos', 'ner', 'part', 'document']
-df.to_csv(DATA_PATH / 'raw.tsv', sep='\t', header=True, index=False)
+
+def to_conll_iob(df: pd.DataFrame):
+    prev_row = tuple()
+    result = pd.Series(index=df.index)
+    for row in df.itertuples():
+        ner = row.ner
+        prev_ner = prev_row.ner if len(prev_row) > 0 else None
+        ind = row.Index
+        if ner != 'O':
+            if prev_ner is None:
+                result[ind] = "B-" + ner
+            elif prev_ner == ner:
+                result[ind] = "I-" + ner
+            else:
+                result[ind] = "B-" + ner
+        else:
+            result[ind] = ner
+        prev_row = row
+
+    df['iob_ner'] = result
+    df.drop('ner', axis=1, inplace=True)
+    return df
+
+
+if __name__ == '__main__':
+    RAW_DATA_PATH = Path('./gmb-2.2.0 - Copy/data')
+    DATA_PATH = Path('./data')
+    df = load_data(RAW_DATA_PATH)
+    df.to_csv(DATA_PATH / 'processed.tsv', sep='\t', header=True, index=False, columns=['token', 'pos', 'lemma',
+                                                                                        'iob_ner', 'part', 'document',
+                                                                                        'sentence'])
