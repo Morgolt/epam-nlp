c57669b9acb889e62464c418585616cc86fa47a1
Rodion Martynov <mar.rodion@gmail.com>
2018-08-27 17:17:31 +0300

    Utility visualizations

---
+++ epam_nlp/util.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from wordcloud import WordCloud
+
+
+def get_wordcloud(tokens: pd.Series, title=None):
+    wordcloud = WordCloud(collocations=False, width=1200, height=800).generate_from_frequencies(tokens.value_counts())
+    _, ax = plt.subplots(figsize=(12, 8))
+    ax.imshow(wordcloud, interpolation='bilinear')
+    if title is not None:
+        ax.title(title)
+    ax.axis("off")
+    return ax
